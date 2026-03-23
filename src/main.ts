718bffe02e2435508b3da45d90492e026b482a46
Andrei <banakh@Andreis-MacBook-Air.local>
2026-03-23 19:47:51 +0300

    feat

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,15 @@ import { DatabaseService } from './modules/database/database.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const allowedOrigins = [
+    'http://localhost:5173',
+    'http://localhost:3000',
+    'http://127.0.0.1:5173',
+    process.env.FRONTEND_URL,
+  ].filter(Boolean);
+  
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173'],
+    origin: allowedOrigins,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
